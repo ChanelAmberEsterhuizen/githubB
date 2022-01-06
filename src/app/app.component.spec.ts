105653c08333551fc9802281c424543872501028
CHANEL <71224645+ChanelBerma108@users.noreply.github.com>
2022-01-06 15:56:16 +0200

    title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'githubA'`, () => {
+  it(`should have as title 'githubB'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('githubA');
+    expect(app.title).toEqual('githubB');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('githubA app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('githubB app is running!');
   });
 });
